03e63b7e714cb82483eaa5686db4d94c46b09ea0
sporkupine <coffeechief25@gmail.com>
2022-03-10 14:50:50 -0600

    finish last enumerable exercise (finally)

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -36,5 +36,11 @@ def find_word_lengths(word_list)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
 
+    word_list.reduce(Hash.new(0)) do |hash,word| 
+      hash[word] = word.length
+      hash
+    end
+
+  
 
 end
